1c257855ed4961b490c43025d851949286e954c9
Digao <165202041+Digao-paiva@users.noreply.github.com>
2024-11-18 17:55:10 -0300

    updates

--- SecondDelivery/HTML CSS JS/hamburguer.js
+++
@@ -1,8 +0,0 @@
-// Seleciona o ícone do hambúrguer e o menu
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
-
-// Adiciona um evento de clique no hambúrguer para alternar a classe 'open'
-hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('open');
-});
